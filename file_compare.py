2ea124085c34cba4f3975890842f13f10f234a95
wangbin_vendor@sensetime.com <wangbin_vendor@sensetime.com>
2020-02-27 11:16:03 +0800

    update

--- file_compare.py
+++ file_compare.py
@@ -3,8 +3,8 @@ import argparse
 
 
 def read_file(filename):
-    with open(filename, 'r') as f:
-        read = f.read()
+    with open(filename, 'r', encoding="utf-8") as f:
+        read = f.readlines()
     return read
 
 
@@ -21,7 +21,7 @@ def main(file_a, file_b):
 
 if __name__ == "__main__":
     arg = argparse.ArgumentParser("file compare tools")
-    arg.add_argument("-a", "file_a", type=str, dest=fa, required=True, help="A file name")
-    arg.add_argument("-b", "file_b", type=str, dest=fb, required=True, help="B file name")
+    arg.add_argument("-a", "--file_A", type=str, dest="fa", required=True, help="A file name")
+    arg.add_argument("-b", "--file_B", type=str, dest="fb", required=True, help="B file name")
     opt = arg.parse_args()
     main(opt.fa, opt.fb)
